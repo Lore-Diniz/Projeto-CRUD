0e8eb9293fc5f750c448fabde07e1648ce5505fe
amsouza19-cloud <am.souza19@gmail.com>
2025-09-11 14:54:30 +0000

    Atualizei o main.py

--- main.py
+++ main.py
@@ -45,3 +45,14 @@ def menu_principal():
 
 if __name__ == "__main__":
     menu_principal()
+
+    def ver_livros_disponiveis(lista_livros):
+    if not lista_livros:
+        print("Nenhum livro disponível no momento.\n")
+        return
+
+    print("--------------------------------------------------")
+    print("LIVROS DISPONÍVEIS PARA LEITURA")
+    for livro in lista_livros:
+        print(f"{livro['id']} - {livro['titulo']} ({livro['ano']}) - Autor: {livro['autor']} - Gênero: {livro['genero']}")
+    print("--------------------------------------------------\n")
